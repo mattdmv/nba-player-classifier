61e52918c8f4f70acaad32104643eb2da136dfba
mattdmv <matija.domovic6@gmail.com>
2021-01-30 21:52:03 +0100

    added option to recognize 2 players on picture

--- client/app.js
+++ client/app.js
@@ -26,24 +26,44 @@ function init() {
             console.log(data);
             if (!data || data.length==0) {
                 $("#resultHolder").hide();
+                $("#resultHolder2").hide();
                 $("#divClassTable").hide();                
                 $("#error").show();
                 return;
             }
             let players = ["giannis_antetokounmpo", "stephen_curry", "luka_doncic", "kevin_durant", "lebron_james"];
             
-            let match = null;
+            let match1 = null;
+            let match2 = null;
+        
+            if (data.length==2) {
+                match1=data[0];
+                match2=data[1];   
+            
+                $("#error").hide();
+                $("#divClassTable").hide();
+                $("#resultHolder").show();
+                $("#resultHolder2").show();
+                $("#resultHolder").html($(`[data-player="${match1.class}"`).html());
+                $("#resultHolder2").html($(`[data-player="${match2.class}"`).html());
+            }
+
             let bestScore = -1;
-            for (let i=0;i<data.length;++i) {
-                let maxScoreForThisClass = Math.max(...data[i].class_probability);
-                if(maxScoreForThisClass>bestScore) {
-                    match = data[i];
-                    bestScore = maxScoreForThisClass;
+
+            if (data.length==1) {
+                for (let i=0;i<data.length;++i) {
+                    let maxScoreForThisClass = Math.max(...data[i].class_probability);
+                    if(maxScoreForThisClass>bestScore) {
+                        match = data[i];
+                        bestScore = maxScoreForThisClass;
+                    }
                 }
             }
+
             if (match) {
                 $("#error").hide();
                 $("#resultHolder").show();
+                $("#resultHolder2").hide();
                 $("#divClassTable").show();
                 $("#resultHolder").html($(`[data-player="${match.class}"`).html());
                 let classDictionary = match.class_dictionary;
@@ -67,6 +87,7 @@ $(document).ready(function() {
     console.log( "ready!" );
     $("#error").hide();
     $("#resultHolder").hide();
+    $("#resultHolder2").hide()
     $("#divClassTable").hide();
 
     init();
